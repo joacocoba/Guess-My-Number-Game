1df220abdc864c8a8f759361546531d705090cf4
joacocoba <42075317+sistemasflow@users.noreply.github.com>
2021-05-23 19:10:07 -0300

    Finish doing game :)

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highScore = 0;
+
+
+function mostrarMensaje(message) {
+    return document.querySelector(".message").textContent = `${message}`;
+}
+
+document.querySelector(".check").addEventListener("click", function(){
+    let guess = Number(document.querySelector(".guess").value);
+    
+    //Comprobar si se ingreso un número
+    if(!guess){
+        mostrarMensaje("Didn't receive a number ⛔️");
+    }
+
+    //Comprobar si acertó el numero secreto.
+    else if(guess === secretNumber){
+        mostrarMensaje("Winner! You guessed the number 🎉");
+        document.querySelector(".number").textContent = secretNumber;
+        document.querySelector(".number").style.width = "30rem";
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        
+        if(score > highScore) {
+            highScore = score;
+            document.querySelector(".highscore").textContent = highScore;
+        }
+    }
+
+    else if(guess !== secretNumber) {
+    if(score > 1) {
+        score--;
+        document.querySelector(".score").textContent = score;
+        mostrarMensaje(guess > secretNumber ? "Too high 📈" : "Too low 📉");
+    } else {
+        mostrarMensaje("You've lost the game ❌")
+        document.querySelector(".score").textContent = 0;
+    }
+    } 
+})
+
+document.querySelector(".again").addEventListener("click", function(){
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    score = 20;
+    document.querySelector(".score").textContent = score;
+
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector("body").style.backgroundColor = "#222";
+})
